d0023be87c5959a393bc23cf1c01677aae49e384
Mohammed Sufiyan <mohammedskhan92@gmail.com>
2026-06-21 16:49:56 +0530

    feat: registered order-service, inventory-service, api-gateway with Eureka

--- order-service/src/main/java/com/order_service/OrderServiceApplication.java
+++ order-service/src/main/java/com/order_service/OrderServiceApplication.java
@@ -2,12 +2,14 @@ package com.order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class OrderServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrderServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
 
 }
